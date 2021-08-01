369a39596a232069e5ec3410d2126816a74b3bf9
Eugenia-gal <galevbor12@gmail.com>
2021-08-01 04:35:10 +0300

    fix useState

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
   const handleClick = e => {
     switch (e.target.id) {
       case 'good':
-        setGood(good + 1);
+        setGood(prevState => prevState + 1);
         break;
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(prevState => prevState + 1);
         break;
       case 'bad':
-        setBad(bad + 1);
+        setBad(prevState => prevState + 1);
         break;
       default:
         return;
